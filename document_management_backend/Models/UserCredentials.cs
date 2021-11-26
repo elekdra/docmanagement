a8c5d3ad8bc83b55bbde1968b5ce2e40d7927900
elekdra <swalitm@gmail.com>
2021-11-26 20:13:04 +0530

    initial commit

---
+++ document_management_backend/Models/UserCredentials.cs
@@ -0,0 +1,15 @@
+
+namespace document_management_backend.Models
+{
+ public class userCredentials
+ {
+  public userCredentials(string username, string password)
+  {
+   this.userName = username;
+   this.passWord = password;
+
+  }
+  public string userName { get; set; }
+  public string passWord { get; set; }
+ }
+}
